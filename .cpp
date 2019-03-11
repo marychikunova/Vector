65088895180dfc5652c158eb34749f5798de6bc9
Mary Chikunova <marychikunova@gmail.com>
2019-03-11 14:43:42 +0300

    Update .cpp

--- .cpp
+++ .cpp
@@ -11,9 +11,11 @@ private:
 public:
   Vector(int ap);
   ~Vector();
-  &cat(int index);
+  Vector& operator = (const Vector& rhs);
   &operator [](unsigned i);
+  unsigned Size();
 };
+Vector operator + (const Vector& a, const Vector& b);
 
 Vector::Vector(int ap) {
   data = new int[ap];
@@ -26,18 +28,79 @@ Vector::~Vector()
   data = NULL;
 };
 
-int& Vector::cat(int index) {
+/*int& Vector::cat(int index) { //не нужно
   assert (0 <= index && index < capacity);
   return data[index];
-};
+};*/
 
 int& Vector::operator [](unsigned index) {
-  return data[index];
+    assert (0 <= index);
+    while(index >= capacity) {
+        capacity *= 2;
+    }
+    int* mas = new int[capacity];
+    for(int i = 0; i < capacity; ++i) {
+        mas[i] = data[i];
+    }
+    data = new int[capacity];
+    for(int i = 0; i < capacity; ++i) {
+        data[i] = mas[i];
+    }
+    delete[]mas;
+    return data[index];
 };
 
+Vector& Vector::operator = (const Vector& rhs) {
+    this -> ~Vector();
+    new(this) Vector(rhs);
+    return *this;
+}
+
+unsigned Vector::Size() {
+    return capacity;
+}
+
+Vector operator + (Vector& a, Vector& b){
+    int n = std::max(a.Size(), b.Size());
+    int m = std::min(a.Size(), b.Size());
+    Vector result(n);
+    for(int i = 0; i <= m; ++i) {
+        result[i] = a[i] + b[i];
+    }
+    if(a.Size() > b.Size()) {
+        for(int i = m + 1; i <= n; ++i) {
+            result[i] = a[i];
+        }
+    }
+    else if(a.Size() < b.Size()) {
+        for(int i = m + 1; i <= n; ++i) {
+            result[i] = b[i];
+        }
+    }
+    return result;
+}
+
 int main() {
-    Vector V(10);
-    V[0] = 1;
-    std::cout << V[0];
+    Vector V1(5);
+    V1[0] = 1;
+    std::cout << V1[0] << " ";
+    std::cout << "\n";
+
+    for(int i = 0; i < 5; ++i)
+        V1[i] = i + 1;
+    for(int i = 0; i < 5; ++i)
+        std::cout << V1[i] << " ";
+    std::cout << "\n";
+
+    Vector V2(10);
+    for(int i = 0; i < 10; ++i)
+        V2[i] = i + 1;
+    for(int i = 0; i < 10; ++i)
+        std::cout << V2[i] << " ";
+    std::cout << "\n";
+
+    Vector V = V1 + V2;
+    for(int i = 0; i < 10; ++i)
+        std::cout << V[i] << " ";
     return 0;
 }
